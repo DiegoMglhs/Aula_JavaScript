3c395bf207382682faf71178a336d7e8d7cb7598
Diego Magalhães <brandao.diego@hotmail.com>
2020-12-15 22:13:30 -0300

    produção final do curso de JS

---
+++ main.js
@@ -0,0 +1,101 @@
+/*function clicou(){
+    document.getElementById("agradecimento").innerHTML = "<b>Obrigado por Clicar</b>";
+    //console.log(document.getElementById("agradecimento"))
+    //alert("obrigado por clicar.")
+}
+
+function redirecionar(){
+    //window.open("https://digitalinnovation.one/");
+    //window.location.href= "https://digitalinnovation.one/";
+}
+
+function trocar(elemento){
+    //document.getElementById("mousemove").innerHTML="<b>Obrigado por passar o mouse</b>"
+    elemento.innerHTML= "<b>Obrigado por passar o mouse</b>";
+    //alert("trocar texto");
+}
+
+function voltar(elemento){
+    //document.getElementById("mousemove").innerHTML="Passe o mouse aqui"
+    elemento.innerHTML ="Passe o mouse aqui"
+}
+
+function load(){
+    alert("Página carregada")
+}
+
+function funcao_change(elemento){
+    console.log(elemento.value)
+}
+*/
+/*function soma(n1,n2){
+    return n1+n2;
+}
+var validar
+function validaIdade(idade){
+    
+    if (idade>=18){
+        validar = true
+    }else{
+        validar = false
+    }
+    return validar;
+}
+
+
+alert(soma(5,10));
+var idade = prompt("Qual sua idade");
+validaIdade(idade)
+console.log(validar);
+*/
+/*
+var d = new Date(); //data atual
+alert(d.getMonth()+1); 
+alert(d.getSeconds());
+alert(d.getHours());
+alert(d.getMinutes());
+*/
+/*
+for(count=0; count<=5;count++){
+    alert(count);
+}
+*/
+/*var count = 0;
+while(count<=5){
+    console.log(count);
+    count ++;
+}*/
+/*var idade = prompt("Qual a sua idade?")
+if(idade>=18){
+    alert("maior de idade");
+}else{
+    alert("menor de idade");
+};*/
+/*var frutas = [{nome:"maça", cor:"vermeha"}, {nome:"uva", cor:"roxa"}]
+console.log(frutas);
+alert(frutas[1].nome);
+*/
+/*var fruta = {nome:"maça", cor:"vermeha"}
+console.log(fruta.nome);
+alert(fruta.cor);*/
+//var lista = ["maçã", "pera", "laranja"];
+//lista.push("uva") adiciona um item na ultima posição
+//lista.pop(); remove o item na ultima posição
+//console.log(lista.length);
+//console.log(lista.reverse());
+//console.log(lista);
+//console.log(lista.toString()[0]); só imprime a letra m e não o primeiro elemento todo
+//console.log(lista.join(" | ")); tambem converte para string, mas consegue colocar algum elemento entre os elementos antes de transfor em string.
+//-----------------------------------------
+//var nome = "Diego Magalães";
+//var n1 = 5;
+//var n2 = 3;
+//var idade3 = "5";
+//var frase = "japão é o melhor time do mundo";
+//alert(nome+ " tem "+ idade+" anos");
+//alert (idade+idade2); // mesmo sem daclarar que os numeros eram da variavel inteiro o programa identifica isso
+//alert(idade2+idade3); // pelas aspas ele entende idade3 como string.
+//console.log(idade+idade2);
+//console.log(frase.replace("japão", "Brasil"));
+//console.log(frase.toUpperCase());
+//console.log(n1*n2);
